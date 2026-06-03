10e0a495089a76467dd8c2d8dbce194205fcd9fa
GreatWhiteShork <48199571+GreatWhiteShork@users.noreply.github.com>
2026-06-03 08:01:59 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,8 +23,8 @@ cam = createCapture({
 facingMode: 'environment' 
    } 
 });
-  tW = cam.width * 3;
-  tH = cam.height * 3;
+  tH = cam.width * 3;
+  tW = cam.height * 3;
 cam.hide(); 
 
 createCanvas(tW, tH);
